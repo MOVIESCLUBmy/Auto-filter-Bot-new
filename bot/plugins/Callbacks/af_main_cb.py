bb17a347a32908c93d90f00c07675c708851fd2b
MOVIESCLUB MY <98048092+MOVIESCLUBmy@users.noreply.github.com>
2022-04-17 17:42:21 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -86,13 +86,13 @@ async def cb_navg(bot, update: CallbackQuery):
 
     else:
         temp_results.append([
-            InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})"),
-            InlineKeyboardButton("Next ⏩", callback_data=f"navigate({index_val}|next|{query})")
+            InlineKeyboardButton("⚜️Click Here To Back⚜️", callback_data=f"navigate({index_val}|back|{query})"),
+            InlineKeyboardButton("⚜️Click Here To Next⚜️", callback_data=f"navigate({index_val}|next|{query})")
         ])
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⭕️ Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} ⭕️", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
