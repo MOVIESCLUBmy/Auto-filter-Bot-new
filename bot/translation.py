0098c33f14fe1a5f81f7da90488421f52c085ab2
MOVIESCLUB MY <98048092+MOVIESCLUBmy@users.noreply.github.com>
2022-06-16 13:23:22 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,7 +48,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/crizzbizzyt">CrizzBizz YT</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/althaf_mr">AF MHMD</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
